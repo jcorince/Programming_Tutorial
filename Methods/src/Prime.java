fc841db34dc0df8a4867340dd7300e66b32153fa
CV-lab <jcorince@yahoo.com>
2016-11-04 17:16:34 +0100

    Solved exercise on prime numbers

--- Methods/src/Prime.java
+++ Methods/src/Prime.java
@@ -3,11 +3,42 @@ import java.util.Scanner;
 
 public class Prime {
 	
+	public static void isPrime(int n)
+	{
+		int result = 0;
+		
+		for(int i=2; i<n;i++)
+		{
+			if (n%i==0)
+			{
+				result = 1;
+			}
+		}//end for				
+				
+		if (result == 1)
+		{
+			System.out.println (n+" is not prime");
+		}else 
+		{
+			System.out.println (n+" is prime");
+		}//end if
+		
+		
+	}//end isPrime method
+	
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Please enter a number");
+		int number=scan.nextInt();
+		
+		isPrime(number);
 		
 		//todo: write a method which tells you if a given number is a prime or not
 		
 	}
+	
+	
 	
 }
