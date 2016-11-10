91864443f52935a0b7c256b13b22f7a8953f4934
CV-lab <jcorince@yahoo.com>
2016-11-11 00:02:41 +0100

    Solved with a recursive method calculating the factorial

--- Recursion/src/Factorial.java
+++ Recursion/src/Factorial.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
 
 public class Factorial {
+	
+	public static long factorial (long n)
+	{
+		if (n == 0 || n == 1) {
+	           return 1;
+	       } else 
+	       {
+	           return (n * factorial(n - 1));
+	       }//end if
+	}// en factorial method
+	
+	public static void main (String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a number.");
+		int number;
+		
+		while(!scan.hasNextLong())
+		{
+			scan.next();
+			System.out.println("Please enter a number greater than zero.");
+		}//end while
+		
+		number = scan.nextInt();
+		
+		System.out.println("The factorial of "+number+" is "+factorial(number));
+		
+		
+		scan.close();
+	}//en main method
 	//todo: implement a recursive functions whicht gives you the factorial value of a given number
 }
