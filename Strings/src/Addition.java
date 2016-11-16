3370908210fb977e3fd171c4f905daba51aadf45
jcorince <joachim.takoutsing@uni-weimar.de>
2016-11-16 21:41:23 +0100

    solved exercise on even and odd numbers

--- Strings/src/Addition.java
+++ Strings/src/Addition.java
@@ -7,6 +7,18 @@ public class Addition {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Please enter an integer");
+		int num1 = scan.nextInt();
+		int sum;
+		do
+		{
+			System.out.println("Please enter another integer");
+			sum = num1+ (scan.nextInt());
+		}while ((scan.next()!='s'));
+		
+		System.out.println("The result is "+ sum);
+		
+		
 		//todo: Read integer values as input and add them until the user writes a 's'.
 		//Print the result.
 		
--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -7,9 +7,37 @@ public class EvenOdd {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		int number;
+		System.out.print ("Please enter a number: ");
+		do
+		{
+			while(!scan.hasNextInt())
+			{
+				scan.next();
+				System.out.println("The number must be an integer.");
+			}//end while
+			number = scan.nextInt();
+			if(number<0)
+			{
+				System.out.println("The number greater than zero.");
+			}//end if			
+		}while(number<0);//end do while
+		
+		
+		
+		System.out.println ("The odd numbers between 0 and "+number+ " are: ");
+		for (int i=1;i<=number;i++)
+		{
+			if (i%2!=0)
+			{
+				System.out.println (i);
+			}//end if
+		}//end for 
+		
+		scan.close();
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
-	}
+	}//end main
 	
-}
+}//end class
