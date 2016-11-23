a4aa210082cbe4b68edd73ed3cdd5d184e7ac22b
jcorince <joachim.takoutsing@uni-weimar.de>
2016-11-23 17:14:15 +0100

    first attemp to solve the problem - work in progress

--- Strings/src/Reverse.java
+++ Strings/src/Reverse.java
@@ -7,6 +7,19 @@ public class Reverse {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Please enter a string");
+		String s = scan.nextLine();
+		int size = s.length();
+		String temp, temp2;
+		int j;
+		
+		for (int i=0;i<size;i++)
+		{
+			j=size-i;
+			temp[i]= s[j];
+			temp2 = temp2 + s[j];
+		}//end for
+		
 		//todo: take an String as input and reverse it. Then print it.
 		
 	}
