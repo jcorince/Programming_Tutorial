4268f39587ec17883a70fed352653402d5a3269d
jcorince <joachim.takoutsing@uni-weimar.de>
2016-12-02 16:48:24 +0100

    safety commits

---
+++ OOP1/src/Student.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+
+public class Student 
+{
+	private String name;
+	private int matrNr;
+	private ArrayList lectures = new ArrayList();
+	
+	public Student(String n, int mN)
+	{
+		this.name = n;
+		this.matrNr = mN;
+	}//end constructor
+	
+	
+	
+	//todo: write a constructor that already gives the student a name and a matriculation number
+	
+	//todo: write getter and setter methods
+	
+	public void addLecture(String lecture)
+	{
+		
+		
+		//todo: fill this method
+	}//end addLecture method
+}//end student class
