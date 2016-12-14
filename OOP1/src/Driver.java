57f659b6bc0372db5448487f0bd373d9551ae062
jcorince <joachim.takoutsing@uni-weimar.de>
2016-12-14 23:49:04 +0100

    bachup

--- OOP1/src/Driver.java
+++ OOP1/src/Driver.java
@@ -1,12 +1,19 @@
 import java.util.*;
 
 public class Driver 
-{
-	
-	
+{	
 	public static void main(String[] args) 
 	{
 		Scanner scan = new Scanner(System.in);
+		
+		Student test = new Student();
+		
+		Professor tP = new Professor();
+		
+		Lecture maths = new Lecture();
+		
+		test.setLecture(maths);
+		tp.setLecture(maths);
 		
 		
 		
@@ -16,4 +23,5 @@ public class Driver
 		//todo: create at least 1 student, 1 professor and one lecture
 		// add the lecture to the student and the professor
 	}//end main method
+	
 }//end  driver class
