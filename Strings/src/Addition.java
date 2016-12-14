7e0c79e9b6fb3160768488c298b64f018da2a792
jcorince <joachim.takoutsing@uni-weimar.de>
2016-12-14 20:42:27 +0100

    from 3 weeks ago

--- Strings/src/Addition.java
+++ Strings/src/Addition.java
@@ -1,28 +1,83 @@
-import java.util.Scanner;
+import java.util.*;
 
 //beginner
 
 public class Addition {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);		
 		
-		System.out.println("Please enter an integer");
-		int num1 = scan.nextInt();
-		int sum;
+		int num1, num2;
+		String intent;
+		int test = 0;
+		int sum = 0;
+		
+		System.out.println("Enter an integer");
 		
 		do
 		{
-			System.out.println("Please enter another integer");
-			sum = num1+ (scan.nextInt());
-		}while ((scan.next()!='s'));
+			while(!scan.hasNextInt())
+			{
+				scan.next();
+				System.out.println("You must enter an integer.");
+			}//end while loop
+			num1 = scan.nextInt();
+			if(num1<0)
+			{
+				System.out.println("The integer must be positive.");
+			}//end if
+		}while(num1<0);	//end do while loop	
 		
-		System.out.println("The result is "+ sum);
+		sum += num1;
 		
+		while(test == 0)
+		{			
+			System.out.println("Enter another integer");
+			
+			do
+			{
+				while(!scan.hasNextInt())
+				{
+					scan.next();
+					System.out.println("You must enter an integer.");
+				}//end while loop
+				num2 = scan.nextInt();
+				if(num2<0)
+				{
+					System.out.println("The integer must be positive.");
+				}//end if
+			}while(num1<0); //end do while
+			
+			sum += num2;
+			
+			do
+			{
+				System.out.println("Enter y to continue or s to display the sum of all previously entered numbers and exit the program.");
+			
+				intent = scan.next();
+				//System.out.println("Intent is now "+intent);
+				
+				if (intent.equalsIgnoreCase("s"))
+				{
+					System.out.println("The result is "+ sum);
+					System.exit(-1);
+				}else if (intent.equalsIgnoreCase("y"))
+				{
+					break;
+				}else
+				{
+					System.out.println("You must enter either y or s only.");					
+				}//end if;
+				
+			}while ((!intent.equalsIgnoreCase("s"))||(!intent.equalsIgnoreCase("y")));//end do while 			
+			
+		}//end while	
+		
+		scan.close();
 		
 		//todo: Read integer values as input and add them until the user writes a 's'.
 		//Print the result.
 		
-	}
+	}//end main method
 	
-}
+}//end addition class
